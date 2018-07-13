3479b313026e378e2c1d58bc5fe4ad01b061d58a
Mathew Grabau <mgrabau@mathewgrabau.ca>
2018-07-13 00:35:44 -0500

    Got some event handlers wired in, partial functionality now.

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,57 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double _lastNumber, _result;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            ClearButton.Click += ClearButton_Click;
+            NegativeButton.Click += NegativeButton_Click;
+            PercentButton.Click += PercentButton_Click;
+            EqualsButton.Click += EqualsButton_Click;
+        }
+
+        private void EqualsButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void PercentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out _lastNumber))
+            {
+                _lastNumber = _lastNumber / 100;
+                ResultLabel.Content = _lastNumber.ToString();
+            }
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ResultLabel.Content = "0";
+        }
+
+        private void NegativeButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out _lastNumber))
+            {
+                _lastNumber *= -1;
+                ResultLabel.Content = _lastNumber.ToString();
+            }
+        }
+
+        private void SevenButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (ResultLabel.Content.ToString() == "0")
+            {
+                ResultLabel.Content = "7";
+            }
+            else
+            {
+                ResultLabel.Content = $"{ResultLabel.Content.ToString()}7";
+            }
+
+            _lastNumber = double.Parse(ResultLabel.Content.ToString());
         }
     }
 }
